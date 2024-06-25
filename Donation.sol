600e1bbc5810cbf8367564f2b6f1d9d38a351eb3
truffleschnozzle <87355054+truffleschnozzle@users.noreply.github.com>
2024-06-25 00:48:04 -0700

    Add functions / segregated funds

--- Donation.sol
+++ Donation.sol
@@ -6,6 +6,7 @@ contract Donation {
     address public owner;
     address public administrator;
     address public beneficiary;
+    uint256 public fund;
 
     constructor(address _administrator) {
         owner = msg.sender;
@@ -13,33 +14,48 @@ contract Donation {
     }
 
     function updateAdministrator(address _administrator) external {
-        require(msg.sender == owner, "Only owner can update beneficiary");
+        require(msg.sender == owner, "Only owner can update administrator");
+        require(address(this).balance == 0, "Fundraising in progress");
         administrator = _administrator;
     }
 
     function updateBeneficiary(address _beneficiary) external {
         require(msg.sender == administrator, "Only beneficiary can withdraw funds");
-        require(address(this).balance == 0, "Cannot change beneficiary during fundraising");
+        require(address(this).balance == 0, "Fundraising in progress");
         beneficiary = _beneficiary;
     }
 
+    function setBeneficiaryNull() internal {
+        require(address(this).balance == 0, "Fundraising in progress");
+        beneficiary = address(0);
+        administrator = address(0);
+    }
+
     function deposit() external payable {
-        require(beneficiary != address(0));
+        require(beneficiary != address(0), "No fundraiser in progress");
         uint adminFee = msg.value * 1 / 100;
         (bool sent, ) = payable(administrator).call{value: adminFee}("");
+        fund += (msg.value - adminFee);
         require(sent == true, "Transfer failed");
-        
     }
 
     function withdraw() external {
         require(msg.sender == beneficiary, "Only beneficiary can withdraw");
-        (bool sent, ) = payable(beneficiary).call{value: address(this).balance}("");
+        (bool sent, ) = payable(beneficiary).call{value: fund}("");
         require(sent == true, "Transfer failed");
+        setBeneficiaryNull();
     }
 
     receive() external payable {
+        require(beneficiary != address(0), "No fundraiser in progress");
         uint donationAmount = msg.value * 1 / 100;
-        (bool sent, ) = payable(beneficiary).call{value: donationAmount}("");
+        (bool sent, ) = payable(administrator).call{value: donationAmount}("");
+        require(sent == true, "Transfer failed");
+    }
+    // function blowmeup() is set for development purposes only
+    function blowmeup() external {
+        require(msg.sender == owner, "Must be owner to destroy contract");
+        (bool sent, ) = payable(owner).call{value: fund}("");
         require(sent == true, "Transfer failed");
     }
 }
